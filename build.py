c136b3297de6513a0475d56a82e457da873793e7
Quentin01 <quentin.buathier@epitech.eu>
2017-05-14 01:47:10 +0900

    Use loggers

--- build.py
+++ build.py
@@ -1,30 +1,49 @@
 import argparse
 import builders
-import sys
+import logging
 import os
+import sys
 
 def main():
     # Parse argument
     parser = argparse.ArgumentParser()
-    parser.add_argument("--build-type", dest='build_type', choices=["Release", "Debug"], default="Release")
-    parser.add_argument("--path", "-p", dest='path', default="thirdparty")
+
+    parser.add_argument("--build-type", help='Specify the build type',
+                        dest='build_type', choices=["Release", "Debug"], default="Release")
+    parser.add_argument("--path", "-p", help='Specify the destination directory',
+                        dest='path', default="thirdparty")
+    parser.add_argument('-v', '--verbose', help='Enables verbose output; '
+                        'repeat up to three time for more verbose output',
+                        action='count', default=0)
 
     args = parser.parse_args()
+
+    # Define the logger
+    logger = logging.getLogger()
+
+    # Define the handle of the logger
+    formatter = logging.Formatter('[%(levelname)8s] %(message)s')
+    log_handler = logging.StreamHandler()
+    log_handler.setFormatter(formatter)
+
+    logger.addHandler(log_handler)
+
+    # Define the level of verbosity of the logger
+    levels = [logging.WARNING, logging.INFO, logging.DEBUG]
+    level = levels[min(len(levels) - 1, args.verbose)]
+
+    logger.setLevel(level=level)
 
     # Build directory zip
     if not os.path.isdir(args.path):
         os.mkdir(args.path)
 
     # Build shaderc
-    print("Build Shaderc")
+    logger.info("Build Shaderc")
 
-    shaderc_builder = builders.Shaderc(args)
+    shaderc_builder = builders.Shaderc(args, logger)
     if not shaderc_builder.build():
-        print("Failed to build Shaderc")
-        sys.exit(1)
-
-    if not shaderc_builder.copy_files():
-        print("Failed to copy files from Shaderc")
+        logger.error("Failed to build Shaderc")
         sys.exit(1)
 
 if __name__ == "__main__":
--- builders/Shaderc.py
+++ builders/Shaderc.py
@@ -8,44 +8,35 @@ from git import Repo
 class Shaderc():
     git_uri = "https://github.com/google/shaderc.git"
 
-    def __init__(self, args):
+    def __init__(self, args, logger):
         self.args = args
+        self.logger = logger;
 
     def _clone(self):
-        print(" -- Shaderc: Clone Shaderc main repository")
-        if os.path.isdir("shaderc"):
-            self.repo = Repo("shaderc")
-        else:
-            self.repo = Repo.clone_from(Shaderc.git_uri, "shaderc")
+        self.logger.info("Shaderc: Clone main repository")
+        if not os.path.isdir("shaderc"):
+            Repo.clone_from(Shaderc.git_uri, "shaderc")
 
-        print(" -- Shaderc: Clone googletest repository")
-        if os.path.isdir("shaderc/third_party/googletest"):
-            self.googletest_repo = Repo("shaderc/third_party/googletest")
-        else:
-            self.googletest_repo = Repo.clone_from("https://github.com/google/googletest.git", "shaderc/third_party/googletest")
+        self.logger.info("Shaderc: Clone googletest repository")
+        if not os.path.isdir("shaderc/third_party/googletest"):
+            Repo.clone_from("https://github.com/google/googletest.git", "shaderc/third_party/googletest")
 
-        print(" -- Shaderc: Clone glslang repository")
-        if os.path.isdir("shaderc/third_party/glslang"):
-            self.glslang_repo = Repo("shaderc/third_party/glslang")
-        else:
-            self.glslang_repo = Repo.clone_from("https://github.com/google/glslang.git", "shaderc/third_party/glslang")
+        self.logger.info("Shaderc: Clone glslang repository")
+        if not os.path.isdir("shaderc/third_party/glslang"):
+            Repo.clone_from("https://github.com/google/glslang.git", "shaderc/third_party/glslang")
 
-        print(" -- Shaderc: Clone spirv-tools repository")
-        if os.path.isdir("shaderc/third_party/spirv-tools"):
-            self.spirv_tools_repo = Repo("shaderc/third_party/spirv-tools")
-        else:
-            self.spirv_tools_repo = Repo.clone_from("https://github.com/KhronosGroup/SPIRV-Tools.git", "shaderc/third_party/spirv-tools")
+        self.logger.info("Shaderc: Clone spirv-tools repository")
+        if not os.path.isdir("shaderc/third_party/spirv-tools"):
+            Repo.clone_from("https://github.com/KhronosGroup/SPIRV-Tools.git", "shaderc/third_party/spirv-tools")
 
-        print(" -- Shaderc: Clone spirv-headers repository")
-        if os.path.isdir("shaderc/third_party/spirv-tools/external/spirv-headers"):
-            self.spirv_headers_repo = Repo("shaderc/third_party/spirv-tools/external/spirv-headers")
-        else:
-            self.spirv_headers_repo = Repo.clone_from("https://github.com/KhronosGroup/SPIRV-Headers.git", "shaderc/third_party/spirv-tools/external/spirv-headers")
+        self.logger.info("Shaderc: Clone spirv-headers repository")
+        if not os.path.isdir("shaderc/third_party/spirv-tools/external/spirv-headers"):
+            Repo.clone_from("https://github.com/KhronosGroup/SPIRV-Headers.git", "shaderc/third_party/spirv-tools/external/spirv-headers")
 
         return True
 
     def _compile(self):
-        print(" -- Shaderc: Configure")
+        self.logger.info("Shaderc: Configure")
 
         if not os.path.isdir("shaderc/build"):
             os.mkdir("shaderc/build")
@@ -64,27 +55,18 @@ class Shaderc():
         if subprocess.Popen(cmake_args, cwd="shaderc/build").wait():
              return False
 
-        print(" -- Shaderc: Build")
+        self.logger.info("Shaderc: Build")
         if subprocess.Popen(["cmake", "--build", "."], cwd="shaderc/build").wait():
              return False
 
         return True
 
-    def build(self):
-        if not self._clone():
-            return False
-
-        if not self._compile():
-            return False
-
-        return True
-
-    def copy_files(self):
+    def _copy_files(self):
         shaderc_root_path = os.path.join(self.args.path, "shaderc")
         shaderc_include_path = os.path.join(shaderc_root_path, "include")
         shaderc_library_path = os.path.join(shaderc_root_path, "lib")
 
-        print(" -- Shaderc: Create directories")
+        self.logger.info("Shaderc: Create directories")
 
         if not os.path.isdir(shaderc_root_path):
             os.mkdir(shaderc_root_path)
@@ -95,14 +77,14 @@ class Shaderc():
         if not os.path.isdir(shaderc_library_path):
             os.mkdir(shaderc_library_path)
 
-        print(" -- Shaderc: Copy include files")
+        self.logger.info("Shaderc: Copy include files")
 
         real_include_path = os.path.join(shaderc_include_path, "shaderc")
 
         if not os.path.isdir(real_include_path):
             shutil.copytree("shaderc/libshaderc/include/shaderc", real_include_path)
 
-        print(" -- Shaderc: Copy library")
+        self.logger.info("Shaderc: Copy library")
 
         if platform.system() == "Linux":
             library_file = "shaderc/build/libshaderc/libshaderc_combined.a"
@@ -111,7 +93,18 @@ class Shaderc():
         else:
             return False
 
-
         shutil.copy(library_file, shaderc_library_path)
+
+        return True
+
+    def build(self):
+        if not self._clone():
+            return False
+
+        if not self._compile():
+            return False
+
+        if not self._copy_files():
+            return False
 
         return True
