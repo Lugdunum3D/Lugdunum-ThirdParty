11c059ba6b00ec5947ea01d271f958a257b4f677
Quentin01 <quentin.buathier@epitech.eu>
2017-05-17 17:58:30 +0900

    Fix the build on CircleCi

--- ci/circle_build.sh
+++ ci/circle_build.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 # Clone Lugdunum to get the config file
-cd ..
-
-git clone https://github.com/Lugdunum3D/Lugdunum.git Lugdunum
-cd Lugdunum
+git clone https://github.com/Lugdunum3D/Lugdunum.git ~/Lugdunum
+cd ~/Lugdunum
 git checkout feature-thirdparty-automatization # TODO: Change the branch to dev
 
 # Build the third party for Lugdunum
-cd Lugdunum-ThirdParty
-mkdir build && cd build && python ../build.py -vvv -z $CIRCLE_ARTIFACTS/Lugdunum-ThirdParty-Linux.zip ../../Lugdunum/thirdparty.yml
+cd ~/Lugdunum-ThirdParty
+mkdir build && cd build && python ../build.py -vvv -z $CIRCLE_ARTIFACTS/Lugdunum-ThirdParty-Linux.zip ~/Lugdunum/thirdparty.yml
