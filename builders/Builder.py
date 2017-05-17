11eab0d07f7a5b1f96fd1925914ee19cfe248ed8
Quentin01 <quentin.buathier@epitech.eu>
2017-05-17 17:55:55 +0900

    Correctly call _compile from Builder

--- builders/Builder.py
+++ builders/Builder.py
@@ -7,9 +7,16 @@ class Builder():
         if "uri" not in self.config["repository"]:
             self.config["repository"]["uri"] = self.default_repository_uri
 
+    def _compile(self, build_type):
+        return True
+
     def build(self):
         if not self._clone():
             return False
+
+        for build_type in self.args.build_types:
+            if not self._compile(build_type):
+                return False
 
         if not self._copy_files():
             return False
