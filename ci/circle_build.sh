7772cb2889b5a44486d97525432659b038cbaac1
Quentin01 <quentin.buathier@epitech.eu>
2017-05-17 17:51:33 +0900

    Build zip file on CircleCi and Appveyor

---
+++ ci/circle_build.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Clone Lugdunum to get the config file
+cd ..
+
+git clone https://github.com/Lugdunum3D/Lugdunum.git Lugdunum
+cd Lugdunum
+git checkout feature-thirdparty-automatization # TODO: Change the branch to dev
+
+# Build the third party for Lugdunum
+cd Lugdunum-ThirdParty
+mkdir build && cd build && python ../build.py -vvv -z $CIRCLE_ARTIFACTS/Lugdunum-ThirdParty-Linux.zip ../../Lugdunum/thirdparty.yml
